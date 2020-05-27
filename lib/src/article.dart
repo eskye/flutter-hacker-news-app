80a84d44d2f4dc11da8366178d95cebf51c789c4
Ijatuyitemi194@gmail.com <Ijatuyitemi194@gmail.com>
2020-05-28 00:25:36 +0100

    implemented expansiontile list

--- lib/src/article.dart
+++ lib/src/article.dart
@@ -42,7 +42,7 @@ final articles = [
       commentsCount: 130
   ),
   new Article(
-      text: "but also the leap into electronic typesetting, remaining essentially unchanged",
+      text: "Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
       age: "6 hours",
       domain: "reuters.com",
       by: "Monkay",
@@ -50,7 +50,7 @@ final articles = [
       commentsCount: 110
   ),
   new Article(
-      text: "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages",
+      text: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?",
       age: "1 hours",
       domain: "reuters.com",
       by: "Monok",
